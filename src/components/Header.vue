167e2950bf79847e825fea55d2279fa2ecc43c12
jobarv <jbarrera@airreyservice.com>
2024-04-05 10:35:57 -0600

    Inicio de Proyecto

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <img src="" alt=""> -->
+
+    <h1>Anuar Barrera</h1>
+    <h2>Autismo</h2>
+  </div>
+</template>    
+<style>
+h1{
+  background-color: red;
+  color: yellow;
+  padding: 20px;
+  border-radius: 7px;
+}
+h2{
+  background-color: blue;
+  color: white;
+  padding: 20px;
+  border-radius: 7px;
+}
+</style>
