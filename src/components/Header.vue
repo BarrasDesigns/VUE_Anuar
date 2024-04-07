4215740db526b308872e98a1ac837380314f1698
jobarv <jbarrera@airreyservice.com>
2024-04-07 16:11:59 -0600

    Se agregan titulos y colores base

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,58 @@
 <template>
   <div>
-    <!-- <img src="" alt=""> -->
+    <div class="objectfit">
 
-    <h1>Anuar Barrera</h1>
-    <h2>Autismo</h2>
+      <img src="../assets/nene1.jpeg" class="fotoNene" alt="fotoNene">
+    </div>
+    <div class="title">
+
+      <h1>Anuar Barrera</h1>
+      <h2 class="fuzzy-bubbles-regular">Autismo</h2>
+    </div>
   </div>
-</template>    
+</template>
 <style>
-h1{
+.title{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+h1 {
   background-color: red;
   color: yellow;
   padding: 20px;
   border-radius: 7px;
 }
-h2{
+
+h2 {
   background-color: blue;
   color: white;
   padding: 20px;
   border-radius: 7px;
+}
+
+/* CUSTOM FOTO */
+.objectfit {
+  display: flex;
+  justify-content: center;
+}
+
+.objectfit>.fotoNene {
+  width: 11rem;
+  height: 11rem;
+  object-fit: cover;
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+  background-color: black;
+  border: 3px solid #b20000;
+}
+
+.nombre_puesto {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
