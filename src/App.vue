2938dddf41fb595d6f1313cc198662d371ef635b
jobarv <jbarrera@airreyservice.com>
2024-04-07 16:41:16 -0600

    primeros estilos en Contacto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <script setup>
 import Header from './components/Header.vue';
+import Contacto from './components/Contacto.vue';
 </script>
 
 <template>
   <div>
     <div id="wrapper">
       <Header />
+      <Contacto />
 
-      
     </div>
   </div>
 </template>
 
-<style scoped>
-  
-</style>
+<style scoped></style>
---
+++ src/components/Contacto.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="title">
+
+            <h4>Contacta a mis padres a los siguientes números</h4>
+        </div>
+
+        <div>
+            <ul>
+
+                <li>
+
+                    <a href="https://wa.me/+528110633292">
+                        <h5>Mi mamí</h5>
+                        <img src="../assets/whatsapp.png" class="icon" alt="whatsappLogo">
+                    </a>
+                </li>
+                <li>
+
+                    <a href="https://wa.me/+528115028945">
+                        <h5>Mi papi</h5>
+                        <img src="../assets/whatsapp.png" class="icon" alt="whatsappLogo">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<style>
+a{
+    text-decoration: none;
+    color: var(--fuentes);
+}
+a:hover{
+    color: var(--hoverFuentes);
+
+}
+li {
+    list-style-type: none;
+    
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    
+}
+li:hover{
+    background-color: var(--hoverEnlaces);
+    border-radius: 50%;
+}
+ul {
+    
+    display: flex;
+    justify-content: center;
+
+}
+
+.icon {
+    width: 50%;
+}
+</style>
